9292b7d55ba5e27dc6dd2c9ac70560120759307d
Jason Loomis <jason@jrloomis.com>
2019-08-15 21:53:38 -0400

    form validated

--- user-onboarding/src/Components/Form.js
+++ user-onboarding/src/Components/Form.js
@@ -14,29 +14,41 @@ const UserForm = ({ touched, errors, status, values }) => {
 
   return (
     <div className="container">
-      <Form className="form">
-        <Field component="input" type="text" name="name" placeholder="Name" className="form__text" />
-        {touched.name && errors.name && <p className="form__error">{errors.name}</p>}
+      <div className="container-signup">
+        <Form className="form">
+          <Field component="input" type="text" name="name" placeholder="Name" className="form__text" />
+          {touched.name && errors.name && <p className="form__error">{errors.name}</p>}
 
-        <Field component="input" type="email" name="email" placeholder="Email" className="form__text" />
-        {touched.email && errors.email && <p className="form__error">{errors.email}</p>}
+          <Field component="input" type="text" name="email" placeholder="Email" className="form__text" />
+          {touched.email && errors.email && <p className="form__error">{errors.email}</p>}
 
-        <Field component="input" type="text" name="password" placeholder="Password" className="form__text" />
+          <Field component="input" type="text" name="password" placeholder="Password" className="form__text" />
+          {touched.password && errors.password && <p className="form__error">{errors.password}</p>}
 
-        <div className="form__check">
-          <label htmlFor="tos" className="form__check--label">
-            Accept Terms of Service
-          </label>
-          <Field type="checkbox" name="tos" checked={values.tos} className="form__check--box" />
-        </div>
+          <div className='container-checkbox'>
+            <div className="form__check">
+              <label htmlFor="tos" className="form__check--label">
+                Accept Terms of Service
+            </label>
+              <Field type="checkbox" name="tos" checked={values.tos} className="form__check--box" />
+            </div>
+            {touched.tos && errors.tos && <p className="form__error">{errors.tos}</p>}
+          </div>
 
-        <button type="submit" className="form__btn">
-          Submit
-        </button>
-      </Form>
-      {users.map(user => (
-        <div key={user.id}>{user.name}</div>
-      ))}
+          <button type="submit" className="form__btn">
+            Submit
+          </button>
+        </Form>
+      </div>
+      <div className="container-card">
+        {users.map(user => (
+          <div key={user.id} className="card">
+            <p className="card__info">{user.name}</p>
+            <p className="card__info">{user.email}</p>
+            <p className="card__info">Thanks for signing up!</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
@@ -51,7 +63,22 @@ export default withFormik({
     };
   },
   validationSchema: Yup.object().shape({
-    name: Yup.string().required("So...what should we call you?")
+    name: Yup
+      .string()
+      .min(2, 'lol no')
+      .max(50, "whoa")
+      .required("So...what should we call you?"),
+    email: Yup
+      .string()
+      .email('Your forgot something')
+      .required('But how would we contact you?'),
+    password: Yup
+      .string()
+      .required('You need to do this')
+      .min(8, '8 character minimum'),
+    tos: Yup
+      .boolean()
+      .oneOf([true], 'Just check the box')
   }),
   handleSubmit(values, { setStatus, resetForm }) {
     axios
